87940073578ffa7efbf39b828dff55fac96a99da
OmarSaad0-0 <89510329+OmarSaad0-0@users.noreply.github.com>
2024-04-30 16:18:49 +0300

    Update TaskScript.sh

--- TaskScript.sh
+++ TaskScript.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
+#Global Variables defination
 User="pet-clinic"
 jdk_url="https://download.oracle.com/java/22/latest/jdk-22_linux-x64_bin.tar.gz"
 jdk_path="/home/pet-clinic/jdk-22.0.1/bin/java"
 java_home="/home/$User"
-#This Command to add new user pet-clinic
+#This Condition to add new user pet-clinic if not existing
 if id "$User" >/dev/null; then
     echo "User exists"
 
@@ -13,7 +14,7 @@ else
     sudo id $User
    
 fi
-
+#This Condition Handles the download of java and installation if not existing
 if ! /home/pet-clinic/jdk-22.0.1/bin/java --version &> /dev/null; then
 	cd $java_home
 	wget $jdk_url
